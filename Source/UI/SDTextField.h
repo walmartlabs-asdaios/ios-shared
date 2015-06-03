bacf6c9cde9945db62487884eb08c849b83a448f
Andrew Finnell <afinnell@walmartlabs.com>
2015-06-03 10:51:34 -0700

    Add ability to SDTextField to resign first responder on a tap outside the field.

--- Source/UI/SDTextField.h
+++ Source/UI/SDTextField.h
@@ -87,6 +87,11 @@ typedef BOOL (^SDTextFieldValidationBlock)(SDTextField *textField);
 @property (nonatomic, assign) BOOL validateWhileTyping;
 
 /**
+ * Check for taps outside field and resign first responder on them
+ */
+@property (nonatomic, assign) BOOL resignFirstResponderOnTapOutside;
+
+/**
  * Performs validation using the validation block on this field and any associated fields (see nextTextField).  If
  * any return FALSE, validateFields will return FALSE as well.  Otherwise, this returns TRUE.
  */
